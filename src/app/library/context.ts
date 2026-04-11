7c7ed4799d893f17a6fe2c11fab81cd118086152
Khairul Hidayat <khai@rul.sh>
2026-04-11 10:44:34 +0800

    fix: scheduler fix

--- src/app/library/context.ts
+++ src/app/library/context.ts
@@ -4,8 +4,10 @@ let library: LibraryItems = [];
 
 export async function rescanLibrary() {
   try {
+    console.log("Scanning library...");
     const res = await scanLibrary([process.env.DATA_PATH || "./data"]);
     library = res;
+    console.log("Library scanned!");
   } catch (err) {
     console.error("Cannot scan library!", err);
   }
--- src/scheduler.ts
+++ src/scheduler.ts
@@ -2,7 +2,7 @@ import { CronJob } from "cron";
 import { rescanLibrary } from "./app/library/context";
 
 export function initScheduler() {
-  new CronJob("*/10 * * * *", rescanLibrary);
+  new CronJob("0 */5 * * * *", rescanLibrary, null, true);
 
   rescanLibrary();
 }
